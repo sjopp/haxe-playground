16f883af55aca0271c5e188cd2132d292d33eb52
Sam Jopp <samjopp@googlemail.com>
2020-01-20 22:21:10 +0000

    Setting up project with moving sprite

---
+++ source/PlayState.hx
@@ -0,0 +1,18 @@
+package;
+
+import flixel.FlxState;
+
+class PlayState extends FlxState {
+
+	var _player: Player;
+
+	override public function create():Void {
+		_player = new Player(20, 20);
+		add(_player);
+		super.create();
+	}
+
+	override public function update(elapsed:Float):Void {
+		super.update(elapsed);
+	}
+}
